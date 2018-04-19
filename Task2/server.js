a330d35b19b7d3fc1f20c21e9e61748cd0198730
dzmitry.alfer <>
2018-04-19 17:18:09 +0300

    Task2: Add server JS

---
+++ Task2/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use('/js', express.static(__dirname + '/dist/js'))
+
+// viewed at http://localhost:3002
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(3002);
