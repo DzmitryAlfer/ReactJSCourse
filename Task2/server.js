3825299c47ca938cc383b7566708603e2955cb92
dzmitry.alfer <>
2018-04-23 11:49:13 +0300

    #Task2: Add webpack dev server

--- Task2/server.js
+++ Task2/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use('/js', express.static(__dirname + '/dist/js'))
+app.use('/js', express.static(__dirname + '/release/js'))
 
 // viewed at http://localhost:3002
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/release/index.html'));
 });
 
 app.listen(3002);
