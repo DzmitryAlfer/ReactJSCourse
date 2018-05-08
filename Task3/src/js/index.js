234571b5ab4cab3d1b6023333a4681f32a28ffe9
dzmitry.alfer <>
2018-05-08 09:45:44 +0300

    #Task3: Add css

--- Task3/src/js/index.js
+++ Task3/src/js/index.js
@@ -1,3 +1,4 @@
+import '../css/index.css'
 import React from 'react'
 import ReactDom from 'react-dom'
 import { DocumentComponent } from './components/DocumentComponent'
--- Task3/webpack.config.js
+++ Task3/webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = function(env, options) {
-    const isProduction = options.mode === "production";
+    const isProduction = options.mode === 'production';
     //console.error(isProduction ? "run webpack in production mode" : "run webpack in development mode");
 
     const config = {
         context: path.resolve(__dirname, 'src'),
-        entry: "./js/index.js",
+        entry: './js/index.js',
 
         output: {
-            path: isProduction ? path.resolve(__dirname, "release") : path.resolve(__dirname, "debug"),
-            filename: "js/index.js"
+            path: isProduction ? path.resolve(__dirname, 'release') : path.resolve(__dirname, 'debug'),
+            filename: 'js/index.js'
         },
 
         resolve: {
-            extensions: [".js", ".jsx", ".json"],
+            extensions: ['.js', '.jsx', '.json']
         },
 
         module:{
@@ -24,23 +25,32 @@ module.exports = function(env, options) {
                     test: /\.(js|jsx)$/,
                     exclude: /node_modules/,
                     use: ['babel-loader']
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                    // use: ExtractTextPlugin.extract({
+                    //   fallback: "style-loader",
+                    //   use: "css-loader"
+                    // })
                 }
             ]
         },
 
         plugins: [
+            new ExtractTextPlugin('[name].css'),
             new HtmlWebpackPlugin({
-            title: "Task2",
-            hash: true,
-            template: "./index.html"
+                title: 'Task2',
+                hash: true,
+                template: './index.html'
             })
         ],
-        
-        mode: isProduction ? "production" : "development",
-        devtool: isProduction ? "none" : "source-map",
+
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'source-map',
 
         watch: true
     };
 
     return config;
-}
+};
