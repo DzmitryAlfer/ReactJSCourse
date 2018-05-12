edfab2ab7c9d135e113f5663a431128183c345b6
dzmitry.alfer <dzmitry.alfer@gmail.com>
2018-05-12 23:52:36 +0300

    #Task3: Add header search components

---
+++ Task3/src/js/components/header/search/FindYourMovieHeaderComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {TextComponent} from "../../TextComponent";
+
+
+export const FindYourMovieHeaderComponent = () => (
+    <div className="header-search-control_item">
+        <TextComponent className="header-text" text="FIND YOUR MOVIE" />
+    </div>
+);
