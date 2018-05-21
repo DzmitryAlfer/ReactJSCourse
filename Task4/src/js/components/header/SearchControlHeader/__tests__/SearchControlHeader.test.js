d16a1ce84569af12aee4708d45228ff0b8110be9
dzmitry.alfer <dzmitry.alfer@gmail.com>
2018-05-21 17:57:53 -0400

    #Task4: Add enzume test

--- Task4/src/js/components/header/SearchControlHeader/__tests__/SearchControlHeader.test.js
+++ Task4/src/js/components/header/SearchControlHeader/__tests__/SearchControlHeader.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import sinon from 'sinon';
 import {SearchControlHeader} from "../index";
 import {mount} from 'enzyme';
 
 describe('SearchControlHeader', () => {
     test('Test button click', () => {
-        const onButtonClick = sinon.spy();
+        const onButtonClick = jest.fn();
         const wrapper = mount((
             <SearchControlHeader onSearchClick={onButtonClick} />
         ));
 
         wrapper.find('.search-button').simulate('click');
-        expect(onButtonClick).to.have.property('callCount', 1);
+        expect(onButtonClick).toHaveBeenCalledWith('');
     });
 });
