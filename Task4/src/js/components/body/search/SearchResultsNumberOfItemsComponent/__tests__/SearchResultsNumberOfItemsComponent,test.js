946c7fb84ed57fa5cc193e76ba707bd2b726e2a3
dzmitry.alfer <dzmitry.alfer@gmail.com>
2018-05-26 22:07:42 -0400

    #Task4: Add tests

---
+++ Task4/src/js/components/body/search/SearchResultsNumberOfItemsComponent/__tests__/SearchResultsNumberOfItemsComponent,test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import {SearchResultsNumberOfItemsComponent} from '../index'
+
+describe('SearchResultsNumberOfItemsComponent', () => {
+    test('Component render1', () => {
+
+        const tree = Renderer.create(
+            <SearchResultsNumberOfItemsComponent numberOfItems={2} author={'Benjamin Franklin'}/>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('Component render2', () => {
+
+        const tree = Renderer.create(
+            <SearchResultsNumberOfItemsComponent author={'Benjamin Franklin'}/>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
--- Task4/src/js/components/body/search/SearchResultsNumberOfItemsComponent/index.jsx
+++ Task4/src/js/components/body/search/SearchResultsNumberOfItemsComponent/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {TextComponent} from "../../common/TextComponent";
+import {TextComponent} from "../../../common/TextComponent/index";
 
 export const SearchResultsNumberOfItemsComponent = ({numberOfItems, author})=> (
     <div className="search-info_item">
