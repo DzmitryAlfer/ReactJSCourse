f111ede867e2794d5efee62fb7c98d676e9ea846
da <>
2018-05-29 20:33:53 +0300

    #Task4: Add first simple test for e2e

--- Task4/webpack.config.js
+++ Task4/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = function(env, options) {
         mode: isProduction ? 'production' : 'development',
         devtool: isProduction ? 'none' : 'source-map',
 
-        watch: true
+        watch: false
     };
 
     return config;
