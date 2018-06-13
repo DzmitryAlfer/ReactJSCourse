620e8a4374bc7e4272526a900c0bdf3aa9d31259
da <>
2018-06-13 16:15:22 +0300

    #Task5: export saga fetch function

--- Task5/src/js/sagas/index.js
+++ Task5/src/js/sagas/index.js
@@ -3,7 +3,7 @@ import * as actions from '../actions'
 import {api} from "../services/api";
 import {getSearchBy, getSortBy} from './selectors'
 
-function* fetchMovies(params = {searchQuery: null}) {
+export function* fetchMovies(params = {searchQuery: null}) {
     const sortBy = yield select(getSortBy);
     const searchBy = yield select(getSearchBy);
     const search = params.searchQuery;
@@ -12,10 +12,12 @@ function* fetchMovies(params = {searchQuery: null}) {
     yield put(actions.movies.receiveMovies(movies));
 }
 
+/* istanbul ignore next */
 export function* watchFetchMovies() {
     yield takeEvery(actions.FETCH_MOVIES, fetchMovies);
 }
 
+/* istanbul ignore next */
 export default function* rootSaga() {
     yield all([fork(fetchMovies), fork(watchFetchMovies)])
 }
