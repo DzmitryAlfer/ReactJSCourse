e4ac932098f260d6dfaf52dc5d3f416f36679bda
da <>
2018-06-14 20:43:40 +0300

    #TAsk6: Add film route

--- Task6/src/js/components/body/search/SearchResultItemComponent/index.jsx
+++ Task6/src/js/components/body/search/SearchResultItemComponent/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {MoviePropType} from '../../../../ProjectPropTypes/MoviePropType';
 import {SearchResultDescriptionComponent} from "../SearchResultDescriptionComponent";
+import {Link} from 'react-router-dom'
 
 export const SearchResultItemComponent = ({movie, onItemClick}) => (
     <div className="search-result_item">
-        <button onClick={() => onItemClick(movie.id)}>
+        <Link to={{pathname:`/film/${movie.id}`}}>
             <img className="search-result-image" src={movie.poster_path}/>
-        </button>
+        </Link>
+        {/*<button onClick={() => onItemClick(movie.id)}>
+            <img className="search-result-image" src={movie.poster_path}/>
+        </button>*/}
         <SearchResultDescriptionComponent title={movie.title} year={(new Date(movie.release_date)).getFullYear()}
                                           genres={movie.genres} rating={movie.vote_average}/>
     </div>
